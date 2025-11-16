6738109c2eca62237500622c4d5455313ab42358
Dancode52 <daniellanehart@gmail.com>
2025-11-16 15:50:46 +0100

    added way to (sort of) read data from file

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include "get_next_line.h"
 
-void get_next_line()
+char	*get_next_line(int fd)
 {
+	static int	i = 0;
+	int	j;
+	char *buf;
+
+	j = 0;
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	while (i < BUFFER_SIZE && buf[j] != '\n')
+	{
+		read(fd, &buf[j], 1);
+		i++;
+		j++;
+	}
+	buf[j] = '\0';
+	j++;
+	return (buf);
 }
