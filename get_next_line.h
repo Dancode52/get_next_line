d9cef889543a83b33e555c0da3267b341d42b667
Dancode52 <daniellanehart@gmail.com>
2025-11-26 10:24:01 +0100

    fixed norm errors

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/26 08:52:15 by dlanehar          #+#    #+#             */
+/*   Updated: 2025/11/26 08:56:04 by dlanehar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 8
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8
 
-#endif
+# endif
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
--- main.c
+++ main.c
@@ -6,74 +6,36 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 08:43:10 by dlanehar          #+#    #+#             */
-/*   Updated: 2025/11/24 15:21:15 by dlanehar         ###   ########.fr       */
+/*   Updated: 2025/11/26 10:21:08 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
-// #include <stdio.h>
-// #include "get_next_line.h"
-
-// int main()
-// {
-// 	char *res;
-// 	int	fd;
-// 	int	i;
-
-// 	fd = open("text.txt", O_RDONLY);
-// 	i = 0;
-// 	while (i < 1)
-// 	{
-// 		res = get_next_line(fd);
-// 		printf("%s", res);
-// 		free(res);
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
 #include "get_next_line.h"
 #include <stdio.h>
 
-int    main(int ac, char **av)
+int	main(void)
 {
-    char    *next_line;
-    int        fd;
-	int i;
-	(void)ac;
-	(void)av;
+	char	*next_line;
+	int		fd;
 
-	i = 0;
- 	fd = open("text.txt", O_RDONLY);
-    if (fd == -1)
-    {
-        write(2, "Error reading file.\n", 20);
-        return (-1);
-    }
-    //close(fd);
-    while (1)
-    {
-        next_line = get_next_line(fd);
-        printf("%s", next_line);
-        if (!next_line)
-        {
-            free(next_line);
-            break ;
-        }
-        free(next_line);
-		i++;
-    }
-    close(fd);
-    return (0);
+	fd = open("text.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		write(2, "Error reading file.\n", 20);
+		return (-1);
+	}
+	while (1)
+	{
+		next_line = get_next_line(fd);
+		printf("%s", next_line);
+		if (!next_line)
+		{
+			free(next_line);
+			break ;
+		}
+		free(next_line);
+	}
+	close(fd);
+	return (0);
 }
-
-// int	main()
-// {
-// 	int fd;
-// 	char* out;
-
-// 	fd = open("text.txt", O_RDONLY);
-// 	out = get_next_line(fd);
-// 	printf("%s", out);
-// 	free(out);
-// }
